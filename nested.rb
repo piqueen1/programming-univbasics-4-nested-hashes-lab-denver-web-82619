9bc304f29ee86d769e4f17f314ce4871007a2910
Finn Thye <piqueen@gmail.com>
2019-07-21 16:23:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,8 @@ def adding_matz
         }
      }
 
+  binding.pry
+
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
